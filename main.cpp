9e65b4da40ee16ccc5614a79737a2cfc2d0777a8
MirFlames <mirflames@yandex.ru>
2020-11-20 12:12:05 +0700

    multiply bux fixes

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
-double my_pow(double x, unsigned int y)
+double my_pow(double x, int y)
 {
     double result = 1;
-    for (int i = 0; i < y; ++i)
+    for (int i = 0; i < abs(y); ++i)
     {
-        result *= x;
+        if (y > 0)
+            result *= x;
+        else if (y < 0)
+            result /= x;
+        else
+            result = 1;
     }
     return result;
 }
